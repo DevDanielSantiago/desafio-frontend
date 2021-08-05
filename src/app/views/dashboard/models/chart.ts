dd1bfe9c6ffdc36b854c39691ac1d3b3bc2ce1b4
DanielSantiago1 <daniel_s_sa@hotmail.com>
2021-08-05 16:32:13 -0400

    adicionando os models do charts

---
+++ src/app/views/dashboard/models/chart.ts
@@ -0,0 +1,10 @@
+export type ChartColorsType = {
+    backgroundColor: string;
+    borderColor: string;
+    pointHoverBackgroundColor: string;
+}
+
+export type ChartDataType = {
+    data: number[];
+    label: string;
+}
