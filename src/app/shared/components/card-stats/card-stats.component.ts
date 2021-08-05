59ab7fdf7c7736923d15932730f61c6ff8dca26b
DanielSantiago1 <daniel_s_sa@hotmail.com>
2021-08-04 20:51:17 -0400

    implementacao component dos cards

--- src/app/shared/components/card-stats/card-stats.component.ts
+++ src/app/shared/components/card-stats/card-stats.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-stats',
   templateUrl: './card-stats.component.html',
-  styleUrls: ['./card-stats.component.scss']
+  styleUrls: ['./card-stats.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardStatsComponent implements OnInit {
 
+  @Input() title: string;
+  @Input() temperature: number;
+  @Input() consumption: number;
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
