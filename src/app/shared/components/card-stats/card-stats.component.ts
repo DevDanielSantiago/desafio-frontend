181f51803fb864c7f9283e06250391eca27c2b67
DevDanielSantiago <dev.danielsantiago@gmail.com>
2021-08-06 16:50:54 -0400

    remocao do scss nao utilizado no componente dos cards

--- src/app/shared/components/card-stats/card-stats.component.ts
+++ src/app/shared/components/card-stats/card-stats.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core
 @Component({
   selector: 'app-card-stats',
   templateUrl: './card-stats.component.html',
-  styleUrls: ['./card-stats.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardStatsComponent implements OnInit {
